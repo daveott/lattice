c7af112b7831ba209a9615e3b6bd011de71eeee1
Dave Ott <dev+daveott@hashrocket.com>
2011-02-27 11:52:31 -0500

    Add spec for SubstitutionProxy

--- lib/lattice/substitution_proxy.rb
+++ lib/lattice/substitution_proxy.rb
@@ -11,7 +11,7 @@ class Lattice::SubstitutionProxy
   end
 
   def and(original)
-    SubstitutionProxy.new(original, header)
+    Lattice::SubstitutionProxy.new(original, header)
   end
 
 end
---
+++ spec/lattice/substitution_proxy_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+describe Lattice::SubstitutionProxy do
+  let(:header) { Lattice::Header.new }
+  let(:data) { {} }
+  let(:substitutions) { data["sub"] = { "<token>" => [1, 2, 3] } }
+  let(:proxy) { Lattice::SubstitutionProxy.new("<token>", header) }
+
+  before do
+    header.stub(:data).and_return(data)
+    header.stub(:substitutions).and_return(substitutions)
+  end
+
+  describe "#with" do
+    subject { header.substitute("<token>").with(1, 2, 3) }
+
+    it "returns itself" do
+      subject.with([1, 2, 3]).should == subject
+    end
+  end
+
+  describe "#and" do
+    subject { proxy.and("<id>") }
+    it { should be_a_kind_of(Lattice::SubstitutionProxy) }
+  end
+end
